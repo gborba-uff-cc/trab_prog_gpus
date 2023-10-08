638fc0f177a3fd86f8ee3d3b4a4142c3190e3ef0
Gabriel Borba <gborba@id.uff.br>
2023-10-08 17:05:54 -0300

    w.i.p. recuperar parametros do json

--- src/c_cpp/simulador_gpu_2.cu
+++ src/c_cpp/simulador_gpu_2.cu
@@ -11,6 +11,19 @@
 cJSON *carregarJSON(
     const char *jsonFilePath
 );
+int carregarParametros(
+    float *h,
+    float *k,
+    int   **posicoesGrade,
+    int   **conexoes,
+    float **condicoesContorno,
+    const cJSON *json
+);
+int descobrirTamanhoMatriz(
+    size_t *bufferNumeroLinhas,
+    size_t *bufferNumeroColunas,
+    cJSON *const matriz
+);
 
 
 cJSON *carregarJSON(const char *jsonFilePath)
@@ -52,6 +65,107 @@ cJSON *carregarJSON(const char *jsonFilePath)
     return jsonStruct;
 }
 
+// REVIEW - testar
+int carregarParametros(
+    float *h,
+    float *k,
+    int   **posicoesGrade,
+    int   **conexoes,
+    float **condicoesContorno,
+    const cJSON *json
+)
+{
+    if (json == NULL) {
+        return 1;
+    }
+
+    const cJSON *_h = cJSON_GetObjectItemCaseSensitive(json, "x_dist");
+    const cJSON *_k = cJSON_GetObjectItemCaseSensitive(json, "y_dist");
+    cJSON *const _posicoes = cJSON_GetObjectItemCaseSensitive(json, "ij_pos");
+    cJSON *const _conexoes = cJSON_GetObjectItemCaseSensitive(json, "connect");
+    cJSON *const _condCont = cJSON_GetObjectItemCaseSensitive(json, "boundary_condition");
+
+    if (
+        !cJSON_IsNumber(_h) ||
+        !cJSON_IsNumber(_k) ||
+        !cJSON_IsArray(_posicoes) ||
+        !cJSON_IsArray(_conexoes) ||
+        !cJSON_IsArray(_condCont)
+    ) {
+        return 2;
+    }
+//
+    size_t linhasPosicoes = 0, colunasPosicoes = 0;
+    size_t linhasConexoes = 0, colunasConexoes = 0;
+    size_t linhasCondCont = 0, colunasCondCont = 0;
+
+    int erro;
+    erro = descobrirTamanhoMatriz(&linhasPosicoes, &colunasPosicoes, _posicoes);
+    if (erro) {
+        return 3;
+    }
+    erro = descobrirTamanhoMatriz(&linhasConexoes, &colunasConexoes, _conexoes);
+    if (erro) {
+        return 4;
+    }
+    erro = descobrirTamanhoMatriz(&linhasCondCont, &colunasCondCont, _condCont);
+    if (erro) {
+        return 5;
+    }
+//
+    int *bufferPosicoes = (int *)     malloc(linhasPosicoes*colunasPosicoes*sizeof(int));
+    if (bufferPosicoes == NULL) {
+        return 6;
+    }
+    int   *bufferConexoes = (int *)   malloc(linhasConexoes*colunasConexoes*sizeof(int));
+    if (bufferConexoes == NULL) {
+        return 7;
+    }
+    float *bufferCondCont = (float *) malloc(linhasCondCont*colunasCondCont*sizeof(float));
+    if (bufferCondCont == NULL) {
+        return 8;
+    }
+//
+    *h = cJSON_GetNumberValue(_h);
+    *k = cJSON_GetNumberValue(_k);
+    *posicoesGrade     = bufferPosicoes;
+    *conexoes          = bufferConexoes;
+    *condicoesContorno = bufferCondCont;
+    return 0;
+}
+
+// REVIEW - testar
+int descobrirTamanhoMatriz(
+    size_t *bufferNumeroLinhas,
+    size_t *bufferNumeroColunas,
+    cJSON *const matriz
+)
+{
+    cJSON *linha = NULL, *coluna = NULL;
+    size_t l = 0, c = 0, c0 = 0;
+
+    // numero de colunas na linha 0
+    cJSON_ArrayForEach(coluna, matriz->child) {
+        c0++;
+    }
+
+    // numero de linhas
+    cJSON_ArrayForEach(linha, matriz) {
+        c = 0;
+        // numero de colunas
+        cJSON_ArrayForEach(coluna, linha) {
+            c++;
+        }
+        // verifica se todas as colunas tem mesmo tamanho
+        if (c != c0) {
+            return 1;
+        }
+        l++;
+    }
+    *bufferNumeroLinhas  = l;
+    *bufferNumeroColunas = c0;
+    return 0;
+}
 
 int main(
     int argc,
