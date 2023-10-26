4b7bc509e4b2d6c997ddf34f45030f8974199a65
Gabriel Borba <gborba@id.uff.br>
2023-10-25 23:34:01 -0300

    correcao no script que compila o simulador

--- src/c_cpp/build.ps1
+++ src/c_cpp/build.ps1
@@ -1,2 +1,2 @@
 # comando para compilar o simulador para windows usando o clang
-clang -o .\output\simulador_gpu_2.exe .\simulador_gpu_2.c .\cJSON.c
+clang -o .\output\simulador_gpu_2.exe .\simulador_gpu_2.cu .\cJSON.c
