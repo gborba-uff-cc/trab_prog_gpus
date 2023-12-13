8b7b1fa53d67db7885037a1887b284ae5e765e16
Gabriel Borba <gborba@id.uff.br>
2023-12-13 11:47:20 -0300

    atualizando scripts de build

--- buildSimulador.bash
+++ buildSimulador.bash
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-nvcc -o ./output/simulador.bin ./src/c_cpp/main.cu ./src/c_cpp/utils.cu ./src/c_cpp/simulador_gpu_2.cu ./src/c_cpp/cJSON.c -lcublas -lcusolver
+nvcc -o ./output/simulador.bin ./src/c_cpp/main.cu ./src/c_cpp/cJSON.c ./src/c_cpp/utils.cu ./src/c_cpp/simulador_gpu_1.cu ./src/c_cpp/simulador_gpu_2.cu -lcublas -lcusolver
--- buildTestSimulador.bash
+++ buildTestSimulador.bash
@@ -1,3 +1,2 @@
 #! /usr/bin/bash
-nvcc -o ./output/simuladorTeste.bin ./src/test/test.cu ./src/c_cpp/utils.cu ./src/c_cpp/simulador_gpu_2.cu ./src/c_cpp/cJSON.c -lcublas -lcusolver
-
+nvcc -o ./output/simuladorTeste.bin ./src/test/test.cu  ./src/c_cpp/cJSON.c ./src/c_cpp/utils.cu ./src/c_cpp/simulador_gpu_1.cu ./src/c_cpp/simulador_gpu_2.cu -lcublas -lcusolver
